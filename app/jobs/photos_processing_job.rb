9c47d467b874415ca5af3625ad469aae58400837
Eslam Mostafa <talvikahvi@gmail.com>
2022-05-05 15:52:00 +0300

    Read & store photos' mime_type on processing

--- app/jobs/photos_processing_job.rb
+++ app/jobs/photos_processing_job.rb
@@ -7,17 +7,24 @@ class PhotosProcessingJob < ApplicationJob
     Dir.mkdir("tmp/saved_photos") unless File.exists?("tmp/saved_photos")
 
     Photo.unprocessed.find_each do |photo|
-      begin
-        data = URI.open(photo.url)
-      rescue OpenURI::HTTPError
-        Rails.logger.error("Error opening Photo ID #{photo.id}.")
-        next
-      end
-
-      filename = data.base_uri.to_s.split("/").last
-      IO.copy_stream(data, File.join("tmp/saved_photos/", filename))
-
-      photo.update!(processed_at: Time.now)
+      process_photo(photo)
     end
+  end
+
+  private def process_photo(photo)
+    begin
+      data = URI.open(photo.url)
+    rescue OpenURI::HTTPError
+      Rails.logger.error("Error opening Photo ID #{photo.id}.")
+      return false
+    end
+
+    filename = data.base_uri.to_s.split("/").last
+    IO.copy_stream(data, File.join("tmp/saved_photos/", filename))
+
+    photo.update!(
+      processed_at: Time.now,
+      mime_type: data.meta.dig("content-type")
+    )
   end
 end
