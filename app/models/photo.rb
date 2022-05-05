d5e59f1f09969b4e674f783569056c1a8076a5da
Eslam Mostafa <talvikahvi@gmail.com>
2022-05-05 14:29:33 +0300

    Add method to import photos & validate photo URL format

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,24 @@
 class Photo < ApplicationRecord
+  validate :url_format
+
+  def self.import_photos(urls)
+    Photo.transaction do
+      urls.each do |url|
+        photo = Photo.new(url:)
+
+        if photo.valid?
+          photo.save!
+        else
+          return false
+        end
+
+      end
+    end
+  end
+
+  private def url_format
+    if url.blank? || !url.match(URI::DEFAULT_PARSER.make_regexp(%w[http https]))
+      errors.add(:url, "Invalid URL: '#{url}'")
+    end
+  end
 end
