fbcdd774d3c87eb2974b32f43e21518809b561ab
Eslam Mostafa <talvikahvi@gmail.com>
2022-05-05 14:30:19 +0300

    Add POST /photos endpoint to take list of URLs and store them as Photos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::API
 
   before_action :authenticate!
 
+  def photos
+    if Photo.import_photos(photos_params[:photo_urls])
+      render json: "Photos created.", status: :ok
+    else
+      render json: "Invalid URLs.", status: :unprocessable_entity
+    end
+  end
+
   def current_user
     @current_user ||= authenticate
   end
@@ -11,5 +19,9 @@ class ApplicationController < ActionController::API
     authenticate_or_request_with_http_token do |token, _options|
       User.find_by(token: token)
     end
+  end
+
+  private def photos_params
+    params.permit(photo_urls: [])
   end
 end
